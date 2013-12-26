b406a5d8375d97675e8d16f5b331aed40dc75997
Brett Francis <brettf@amazon.com>
2013-12-26 14:42:38 -0800

    updated to optional shard_count

--- long-poster.sh
+++ long-poster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python poster.py my-first-stream 10 --poster_count 50 --poster_time 345600 --partition_key Kinesis02 --quiet &
+nohup python poster.py my-first-stream --shard_count 10 --poster_count 50 --poster_time 345600 --partition_key Kinesis02 --quiet &
